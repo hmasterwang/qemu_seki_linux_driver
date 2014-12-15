c3134deaba7ef767e12cff3f6d093cb09cf077e3
Afa <alpha@tomatoeskit.org>
2014-12-15 13:48:07 -0500

    MMIO support skeleton added.

--- seki_driver.c
+++ seki_driver.c
@@ -30,6 +30,8 @@
 #include <linux/module.h>
 #include <linux/pci.h>
 #include <linux/printk.h>
+#include <linux/io.h>
+#include <linux/slab.h>
 
 MODULE_LICENSE("Dual MIT/GPL");
 MODULE_AUTHOR("Afa.L Cheng <afa@afa.moe>");
@@ -41,23 +43,76 @@ static const struct pci_device_id seki_dev_idtbl[] = {
     {}
 };
 
+typedef struct SekiData {
+    unsigned long phyical_addr;
+    void *virtual_addr;
+    unsigned long mmio_length;
+} SekiData;
+
+static SekiData *_driver_device_data = 0;
+// This is rather ugly, we'll make a better one later
+
+MODULE_DEVICE_TABLE(pci, seki_dev_idtbl);
+
+
 static int seki_probe(struct pci_dev *dev, const struct pci_device_id *did)
 {
     int rv;
+    SekiData *device_data;
 
     pr_debug("Device Found");
 
-    rv = pci_enable_device(dev);
+    device_data = kzalloc(sizeof(SekiData), GFP_KERNEL);
+    if (!device_data) {
+        pr_err("Failed to allocate SekiData\n");
+        return -ENOMEM;
+    }
+    _driver_device_data = device_data;
 
+    rv = pci_enable_device(dev);
     if (rv)
         return rv;
 
+
+    pci_set_master(dev);
+
+    if (pci_request_region(dev, 0, "seki_emu")) {
+        pr_err("Failed to request region\n");
+
+        goto err_disable;
+    };
+
+    device_data->phyical_addr = pci_resource_start(dev, 0);
+    device_data->mmio_length  = pci_resource_len(dev, 0);
+    device_data->virtual_addr = ioremap_nocache(device_data->phyical_addr, device_data->mmio_length);
+
+    pr_debug("Virt Addr: %p\n", device_data->virtual_addr);
+
     return 0;
+
+err_disable:
+    pci_disable_device(dev);
+    kfree(device_data);
+    _driver_device_data = 0;
+
+    return rv;
 }
 
 static void seki_remove(struct pci_dev *dev) {
-    pr_debug("Device removed\n");
 
+    if (_driver_device_data->virtual_addr)
+        iounmap(_driver_device_data->virtual_addr);
+
+    pci_release_region(dev, 0);
+
+    pci_clear_master(dev);
+
+    pci_disable_device(dev);
+
+    kfree(_driver_device_data);
+    _driver_device_data = 0;
+
+    pr_debug("Device removed\n");
 }
 
 static int seki_suspend(struct pci_dev *dev, pm_message_t state)
